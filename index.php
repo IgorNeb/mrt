f379ad7ecadd4fa10726eb925d1669528d461677
Igor Pershko <globaltelnet7.62@gmail.com>
2017-06-29 01:29:54 +0300

    one

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>MRT</title>
+
+    <!-- Bootstrap -->
+    
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/mycss.css" type="text/css">
+	
+  </head>
+  
+    <body>
+  	
+  	  <header>
+          
+      	<div class="action"> 
+          <div class="container text-center">
+      		АКЦИЯ! СКИДКА 10% С ПОНЕДЕЛЬНИКА ПО СУББОТУ С 19:00 - 21:00 В ВОСКРЕСЕНЬЕ - ЦЕЛЫЙ ДЕНЬ
+          </div>
+      	</div>
+
+        <div class="logo container">
+
+              <div class="logologo"><img src="pic/1/logo.png" alt="Whitesquare logo"> </div>
+              <div class="logo2"><img src="pic/1/placeholder.png" alt="Whitesquare logo"></div>
+              <div class="logo1"> г.Киев, ул.Вышгородская 69</div>
+              <div class="logo4"><img src="pic/1/smartphone.png" alt="Whitesquare logo"></div>
+              <div class="logo3">(044) 383-88-77</div>
+              <div class="logo6"><img src="pic/1/smartphone.png" alt="Whitesquare logo"></div>
+              <div class="logo5">(044) 430-77-33</div>
+              <div class="logo7"><img src="pic/1/smartphone.png" alt="Whitesquare logo"></div>
+              <div class="logo8">(098) 678-87-87</div>
+              <div class="logo9"><a href="#"> <img src="pic/1/button-21.png" alt="Whitesquare logo"> </a></div>
+            </div>         
+       
+            
+        <div class="ban container">
+          <img src="pic/1/ban-1.png" alt="Whitesquare logo">
+        </div>
+
+    	<div class="ls container">
+
+    		<ul class="list-inline">
+    		  <li><a href=#>МРТ В КИЕВЕ</a></li>
+    		  <li><a href=#>УСЛУГИ</a></li>
+    		  <li><a href=#>ЦЕНЫ</a></li>
+    		  <li><a href=#>КОНТАКТЫ</a></li>
+    		  <li><a href=#>ЗАПИСЬ</a></li>
+    		  <li><a href=#>СОТРУДНИЧЕСТВО</a></li>
+    		  <li><a href=#>ВАКАНСИИ</a></li>
+    		  <li><a href=#>О НАС</a></li>
+    		  <li><a href=#>СТАТЬИ</a></li>
+  		  </ul>
+  	 </div>
+      
+     </header>
+    
+        
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <div id="blk">
+            <div id="man"><img src="pic/1/man.png" alt="Whitesquare logo"></div>
+            <div id="pic1"><img class="man1" src="pic/1/1.png" alt="Whitesquare logo"></div>
+            <div id="pic2"><img class="man2" src="pic/1/2.png" alt="Whitesquare logo"></div>
+            <div id="pic3"><img class="man3" src="pic/1/4.png" alt="Whitesquare logo"></div>
+            <div id="pic4"><img class="man3" src="pic/1/5.png" alt="Whitesquare logo"></div>
+            <div id="pic5"><img class="man3" src="pic/1/6.png" alt="Whitesquare logo"></div>
+            <div id="pic6"><img class="man3" src="pic/1/7.png" alt="Whitesquare logo"></div>
+            <div id="pic7"><img class="man3" src="pic/1/33.png" alt="Whitesquare logo"></div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div id="rg">
+          <div id="magnetic">
+              <p>Магнитно резонансная томография</p>
+          </div>
+          <div class="parag">
+            <p>Диагностический центр "SMART Диагностика" предоставляет высококачественные услуги магнитно резонансной томографии (МРТ).</p>
+
+            <p>Для обследований используется только современная техника, а именно - высокопольный томограф Philips Intera 1.5Т. С его помощью специалисты получают точнейшие результаты, дающие возможность оперативно поставить медицинское заключение. Проводятся обследования как определенных органов тела так и комплексные.</p>
+          </div>
+          <div>
+            <p>SMART Диагностика это </p>
+          </div>
+          <div class="ls1">
+            <p>выгодные и доступые цены</p>
+            <p>проффесиональный персонал</p>
+            <p>результаты в течении 2 часов</p>
+            <p>тщательное отношение к клиентам</p>
+            <p>очень удобное для Вас местоположение</p>
+            <p>быстрая запись на приём + консультация</p>
+          </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="main2 container">
+      Наилучшая МРТ в Киеве
+    </div>
+
+    <div class="container">
+      <div class="row">
+          <div class="col-sm-6 text-center">
+            <p>Магнитно резонансная томография используется врачами во многих странах и городах, в том числе в Киеве. Именно она считается наиболее безопасной и результативной методикой обследования состояния внутренних органов, суставов, мягких тканей и других анатомических зон человеческого организма. С помощью МРТ можно установить четкий диагноз по многим болезням, патологиям и травмам в короткие сроки. Взрослая и детская томография успешно проводиться в Украине, США, Японии, западноевропейских странах.</p>
+
+            <div><img src="pic/2/shield.png"></div>
+            <div><img src="pic/2/shield.png"></div>
+            <div><img src="pic/2/shield.png"></div>
+            <div><img src="pic/2/shield.png"></div>
+            <div><img src="pic/2/shield.png"></div>
+            <div><img src="pic/2/shield.png"></div> 
+            <p>Профессиональный центр "SMART Диагностика" проводит:</p>
+            <P>Холангиопанкреатография (желчевыводящие протоки)</P>
+            <P>Динамические обследования посредством инжектора</P>
+            <P>Антиография сосудов головного мозга и шеи</P>
+            <P>Скрининговые исследования позвоночника</P>
+            <P>Томографию головного мозга</P>
+            <P>Остальные виды МРТ</P>
+          </div>
+          <div class="col-sm-6">
+            <div class="container">
+              <div><img src="pic/2/group.png"></div>
+              <div><img src="pic/2/fon.png"></div>
+              <p>МРТ с контрастом</p>
+              <p>Противопоказания МРТ</p>
+              <p>Специалисты МРТ центра</p>
+              <p>Описание МРТ диагностики</p>
+              <p>Фото МРТ, примеры снимков</p>
+              <p>Лицензия на МРТ исследования</p>
+              <p>Преимущества и недостатки МРТ</p>
+              <p>Диплом центра диагностики МРТ</p>
+            </div>
+
+          </div>
+
+          
+
+      </div>
+    </div>
+
+    <div class="container">
+    А главное - результаты процедуры выдаются максимум через 2 часа, что очень удобно, (при условии, что назначение диагноза не требует врачебного консилиума)!
+    </div>
+
+    <div class="main3 container">
+      Опытные специалисты SMART Диагностика
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6"><img src="pic/3/scanner.png"></div>
+        <div class="col-sm-6">
+          <p>Пройти МРТ намного безопаснее, чем рентген или компьютерную томографию. Дело в том, что последние методики используют вредное излучение, которое хоть и не является смертельным, но ограничивает частоту проведения исследований. МР-томография не подвергает пациента лучевой нагрузке. Если Вы ищете, где лучше сделать МРТ, то стоит зайти в центр "SMART Диагностика". Здесь от Вас не потребуют особенной подготовки к процедуре. В некоторых случаях врач может назначить введение контрастного вещества, если нужна больше информативность при МРТ головного мозга, органов брюшной полости или малого.</p>
+
+<p>При сильных травмах и большинстве болезней, томография в диагностическом центре позволяет точно установить заключение, выявить пострадавшие области мозга. Благодаря быстрому определению диагноза, медики начинают эффективное лечение.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="main3 container">
+      Где можно пройти МРТ в Киеве?
+    </div>
+
+     <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <div>SMART Диагностика - это место, где Вы сможете обследоваться на МРТ быстро и недорого. Записаться на процедуру могут жители и гости Киева. Необходимо только позвонить по контактному телефону и договориться о времени. По сравнение с другими подобными центрами, именно здесь можно дешево сделать МРТ ребенку или взрослому. Наш центр диагностики пользуется популярностью в Киеве, его знают и уважают клиенты с других крупных городов Украины, Германии, Израиля.</div>
+          </div>
+          <div class="col-sm-6">
+            <div><img src="pic/4/manu.png"></div>
+            <div><img src="pic/4/btn.png"></div>
+          </div>
+      </div>
+    
+    </div>
+
+    <div class="container">
+      <div class="main4">Мы поможем Вам !</div> 
+      <div class="tt1">Если Вы ищете, где дешевле сделать МРТ, обращайтесь сразу в "SMART Диагностика". Здесь работают опытные профессионалы своего дела в сфере медицины и современной техники. Они регулярно отслеживают новинки в данном направлении, уникальные разработки, изучают драгоценный опыт иностранных специалистов и ориентируются только на лучшие центры МРТ. Сотрудники отбирают и реализуют на практике исключительно результативные и безопасные методики, чтобы наши пациенты ощутили все преимущества обследования в диагностическом центре.
+
+Сделать МРТ в Киеве по цене такой низкой, как у нас, очень сложно.
+
+Находится центр "SMART Диагностика" по адресу: ул. Вышгородская, 69, Институт эндокринологии и обмена веществ имени В.П. Комиссаренко.
+
+Добраться к нам можно самостоятельно практически с любого района Киева (Троещина, Берестейская, Дарница, Позняки, ЖД вокзала). Записаться на МРТ или задать любые вопросы по процедуре, можно по контактному телефону (044) 383-88-77, (044) 430-77-33, (098) 678-87-87, (063) 574-87-87.</div> 
+      <div class="tt2">SMART Диагностика - это качественная и доступная МРТ-диагностика в Киеве.
+      </div> 
+    </div>
+
+    <div class="main5 container">Наши специалисты</div>
+
+    <div class="container">
+      <div class="row">
+          <div class="col-sm-4">
+            <div><img src="pic/5/foto1.png"></div>
+            <div>Сальникова Елена Святославовна</div>
+            <div>врач-рентгенолог
+высшей категории,
+кандидат медицинских наук</div>
+          </div>
+          <div class="col-sm-4">
+            <div><img src="pic/5/foto2.png"></div>
+            <div>Лавренко Анастасия Юрьевна</div>
+            <div>врач МРТ</div>
+          </div>
+          <div class="col-sm-4">
+            <div><img src="pic/5/foto3.png"></div>
+            <div>Полищук Ирина Николаевна</div>
+            <div>оператор МРТ,
+медсестра-анестезист высшей
+категории</div>
+          </div>
+      </div>
+    </div>
+  	
+<div class="main6 container">Наши партнеры</div>
+
+<div class="container">
+   
+  
+</div>
+
+    <footer>
+      <div class="container">
+        <div class="main7">Контакты</div>
+        <div class="main7">г.Киев, ул. Вышгородская 69</div>
+       
+        <div class="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d20348.224172921324!2d30.5203816!3d50.3939964!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1498484881392" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+      </div>
+      
+    
+  		
+  	</footer>
+
+    </div>	
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.js"></script>
+      <script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+    </body>
+  </html>
+
+  
